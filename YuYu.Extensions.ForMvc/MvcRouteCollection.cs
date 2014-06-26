a0b130fdf5f7c391077864cc4ba3068c09648788
keithen.leo <keithen.leo@gmail.com>
2014-06-26 12:22:34 +0800

    Delete MvcTest

---
+++ YuYu.Extensions.ForMvc/MvcRouteCollection.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace YuYu.Components
+{
+    /// <summary>
+    /// 路由集合类
+    /// </summary>
+    public class MvcRouteCollection : ConfigurationElementCollection
+    {
+        /// <summary>
+        /// 元素名称
+        /// </summary>
+        public const string RouteKey = "route";
+
+        /// <summary>
+        /// 获取下标 index 的路由元素
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public MvcRouteElement this[int index]
+        {
+            get { return this.RouteElements[index]; }
+        }
+
+        /// <summary>
+        /// 获取 System.Configuration.ConfigurationElementCollection 的类型。
+        /// </summary>
+        public override ConfigurationElementCollectionType CollectionType
+        {
+            get { return ConfigurationElementCollectionType.BasicMap; }
+        }
+
+        /// <summary>
+        /// 元素名称
+        /// </summary>
+        protected override string ElementName
+        {
+            get { return RouteKey; }
+        }
+
+        /// <summary>
+        /// 路由元素组
+        /// </summary>
+        public virtual MvcRouteElement[] RouteElements
+        {
+            get { return this.Cast<MvcRouteElement>().ToArray(); }
+        }
+
+        /// <summary>
+        /// 添加路由元素
+        /// </summary>
+        /// <param name="element"></param>
+        public void Add(MvcRouteElement element)
+        {
+            base.BaseAdd(element);
+        }
+
+        /// <summary>
+        /// 移除路由元素
+        /// </summary>
+        /// <param name="element"></param>
+        public void Remove(MvcRouteElement element)
+        {
+            base.BaseRemove(GetElementKey(element));
+        }
+
+        /// <summary>
+        /// 新建元素
+        /// </summary>
+        /// <returns></returns>
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new MvcRouteElement();
+        }
+
+        /// <summary>
+        /// 获取指定配置元素的元素键。
+        /// </summary>
+        /// <param name="element"></param>
+        /// <returns></returns>
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return element;
+        }
+    }
+}
---
+++ YuYu.Extensions.ForWeb/ExtendMethodsForRouteCollection.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Routing;
+
+namespace YuYu.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ExtendMethodsForRouteCollection
+    {
+        /// <summary>
+        /// 注册Route
+        /// </summary>
+        /// <param name="routes"></param>
+        public static void RegisterPageRoutes(this System.Web.Routing.RouteCollection routes)
+        {
+            YuYuWebConfigurationManager.RegisterPageRoutes(routes);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="routes"></param>
+        /// <param name="name"></param>
+        /// <param name="protocol"></param>
+        /// <param name="domain"></param>
+        /// <param name="url"></param>
+        /// <param name="physicalFile"></param>
+        /// <param name="checkPhysicalUrlAccess"></param>
+        /// <returns></returns>
+        public static Route MapDomainPageRoute(this System.Web.Routing.RouteCollection routes, string name, string protocol, string domain, string url, string physicalFile, bool checkPhysicalUrlAccess=false)
+        {
+            return routes.MapDomainPageRoute(name, protocol, domain, url, physicalFile, checkPhysicalUrlAccess, null, null, null);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="routes"></param>
+        /// <param name="name"></param>
+        /// <param name="protocol"></param>
+        /// <param name="domain"></param>
+        /// <param name="port"></param>
+        /// <param name="url"></param>
+        /// <param name="physicalFile"></param>
+        /// <param name="checkPhysicalUrlAccess"></param>
+        /// <returns></returns>
+        public static Route MapDomainPageRoute(this System.Web.Routing.RouteCollection routes, string name, string protocol, string domain, int port, string url, string physicalFile, bool checkPhysicalUrlAccess = false)
+        {
+            return routes.MapDomainPageRoute(name, protocol, domain, port, url, physicalFile, checkPhysicalUrlAccess, null, null, null);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="routes"></param>
+        /// <param name="name"></param>
+        /// <param name="protocol"></param>
+        /// <param name="domain"></param>
+        /// <param name="url"></param>
+        /// <param name="physicalFile"></param>
+        /// <param name="checkPhysicalUrlAccess"></param>
+        /// <param name="defaults"></param>
+        /// <returns></returns>
+        public static Route MapDomainPageRoute(this System.Web.Routing.RouteCollection routes, string name, string protocol, string domain, string url, string physicalFile, bool checkPhysicalUrlAccess , object defaults )
+        {
+            return routes.MapDomainPageRoute(name, protocol, domain, url, physicalFile, checkPhysicalUrlAccess, defaults, null, null);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="routes"></param>
+        /// <param name="name"></param>
+        /// <param name="protocol"></param>
+        /// <param name="domain"></param>
+        /// <param name="port"></param>
+        /// <param name="url"></param>
+        /// <param name="physicalFile"></param>
+        /// <param name="checkPhysicalUrlAccess"></param>
+        /// <param name="defaults"></param>
+        /// <returns></returns>
+        public static Route MapDomainPageRoute(this System.Web.Routing.RouteCollection routes, string name, string protocol, string domain, int port, string url, string physicalFile, bool checkPhysicalUrlAccess , object defaults )
+        {
+            return routes.MapDomainPageRoute(name, protocol, domain, port, url, physicalFile, checkPhysicalUrlAccess, defaults, null, null);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="routes"></param>
+        /// <param name="name"></param>
+        /// <param name="protocol"></param>
+        /// <param name="domain"></param>
+        /// <param name="url"></param>
+        /// <param name="physicalFile"></param>
+        /// <param name="checkPhysicalUrlAccess"></param>
+        /// <param name="defaults"></param>
+        /// <param name="constraints"></param>
+        /// <returns></returns>
+        public static Route MapDomainPageRoute(this System.Web.Routing.RouteCollection routes, string name, string protocol, string domain, string url, string physicalFile, bool checkPhysicalUrlAccess, object defaults, object constraints)
+        {
+            return routes.MapDomainPageRoute(name, protocol, domain, url, physicalFile, checkPhysicalUrlAccess, defaults, constraints, null);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="routes"></param>
+        /// <param name="name"></param>
+        /// <param name="protocol"></param>
+        /// <param name="domain"></param>
+        /// <param name="port"></param>
+        /// <param name="url"></param>
+        /// <param name="physicalFile"></param>
+        /// <param name="checkPhysicalUrlAccess"></param>
+        /// <param name="defaults"></param>
+        /// <param name="constraints"></param>
+        /// <returns></returns>
+        public static Route MapDomainPageRoute(this System.Web.Routing.RouteCollection routes, string name, string protocol, string domain, int port, string url, string physicalFile, bool checkPhysicalUrlAccess, object defaults, object constraints)
+        {
+            return routes.MapDomainPageRoute(name, protocol, domain, port, url, physicalFile, checkPhysicalUrlAccess, defaults, constraints, null);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="routes"></param>
+        /// <param name="name"></param>
+        /// <param name="protocol"></param>
+        /// <param name="domain"></param>
+        /// <param name="url"></param>
+        /// <param name="physicalFile"></param>
+        /// <param name="checkPhysicalUrlAccess"></param>
+        /// <param name="defaults"></param>
+        /// <param name="constraints"></param>
+        /// <param name="dataTokens"></param>
+        /// <returns></returns>
+        public static Route MapDomainPageRoute(this System.Web.Routing.RouteCollection routes, string name, string protocol, string domain, string url, string physicalFile, bool checkPhysicalUrlAccess, object defaults, object constraints, object dataTokens)
+        {
+            return routes.MapDomainPageRoute(name, protocol, domain, 80, url, physicalFile, checkPhysicalUrlAccess, defaults, constraints, dataTokens);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="routes"></param>
+        /// <param name="name"></param>
+        /// <param name="protocol"></param>
+        /// <param name="domain"></param>
+        /// <param name="port"></param>
+        /// <param name="url"></param>
+        /// <param name="physicalFile"></param>
+        /// <param name="checkPhysicalUrlAccess"></param>
+        /// <param name="defaults"></param>
+        /// <param name="constraints"></param>
+        /// <param name="dataTokens"></param>
+        /// <returns></returns>
+        public static Route MapDomainPageRoute(this System.Web.Routing.RouteCollection routes, string name, string protocol, string domain, int port, string url, string physicalFile, bool checkPhysicalUrlAccess, object defaults, object constraints, object dataTokens)
+        {
+            if (routes == null)
+                throw new ArgumentNullException("routes");
+            if (domain == null)
+                throw new ArgumentNullException("domain");
+            if (url == null)
+                throw new ArgumentNullException("url");
+            Route route = new DomainRoute(domain, url, new PageRouteHandler(physicalFile, checkPhysicalUrlAccess))
+            {
+                Defaults = RouteValueDictionaryHelper.CreateRouteValueDictionary(defaults),
+                Constraints = RouteValueDictionaryHelper.CreateRouteValueDictionary(constraints),
+                DataTokens = RouteValueDictionaryHelper.CreateRouteValueDictionary(dataTokens),
+                Protocol = protocol,
+                Port = port,
+            };
+            routes.Add(name, route);
+            return route;
+        }
+    }
+}
