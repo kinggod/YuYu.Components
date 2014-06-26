9768bd707d820d84c70c7d54dfe29f103fd84cf9
keithen.leo <keithen.leo@gmail.com>
2014-06-26 12:17:16 +0800

    Update YuYu.Extensions.ForJavascript

--- YuYu.Extensions.ForJavascript/Scripts/yuyu.extensions.js
+++ YuYu.Extensions.ForJavascript/Scripts/yuyu.extensions.js
@@ -69,23 +69,19 @@ Guid.NewGuid = function () {
     return new Guid(string);
 };
 Date.prototype.format = function (format) {
-    var o = {
-        "M+": this.getMonth() + 1, //month
-        "d+": this.getDate(), //day
-        "h+": this.getHours(), //hour
-        "m+": this.getMinutes(), //minute
-        "s+": this.getSeconds(), //second
-        "q+": Math.floor((this.getMonth() + 3) / 3), //quarter
-        "S": this.getMilliseconds() //millisecond
-    }
-    if (/(y+)/.test(format))
-        format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    for (var k in o)
-        if (new RegExp("(" + k + ")").test(format))
-            format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
-    return format;
+    var z = {
+        y: this.getFullYear(),
+        M: this.getMonth() + 1,
+        d: this.getDate(),
+        h: this.getHours(),
+        m: this.getMinutes(),
+        s: this.getSeconds()
+    };
+    return format.replace(/(y+|M+|d+|h+|m+|s+)/g, function (v) {
+        return ((v.length > 1 ? '0' : '') + eval('z.' + v.slice(-1))).slice(-(v.length > 2 ? v.length : 2));
+    });
 };
-Date.fromJson = function (jsonDate) {
+Date.fromJSON = function (JSONDate) {
     try {
         return new Date(parseFloat(dateString.replace('/Date(', '').replace(')/', '')));
     } catch (exception) {
