c275beb31153157eb24a59852452bdfec1f4b42d
Keithen.Leo <keithen.leo@gmail.com>
2014-09-05 23:53:52 +0800

    YuYu.Extensions.ForJavascripts add some methods in Date

--- YuYu.Extensions.ForJavascript/Scripts/yuyu.extensions.js
+++ YuYu.Extensions.ForJavascript/Scripts/yuyu.extensions.js
@@ -81,6 +81,34 @@ Date.prototype.format = function (format) {
         return ((v.length > 1 ? '0' : '') + eval('z.' + v.slice(-1))).slice(-(v.length > 2 ? v.length : 2));
     });
 };
+Date.prototype.addMillionSeconds = function (millionSeconds) {
+    this.setMilliseconds(this.getMilliseconds() + millionSeconds);
+    return this;
+};
+Date.prototype.addSeconds = function (seconds) {
+    this.setSeconds(this.getSeconds() + seconds);
+    return this;
+};
+Date.prototype.addMinutes = function (minutes) {
+    this.setMinutes(this.getMinutes() + minutes);
+    return this;
+};
+Date.prototype.addHours = function (hours) {
+    this.setHours(this.getHours() + hours);
+    return this;
+};
+Date.prototype.addDays = function (days) {
+    this.setDate(this.getDate() + days);
+    return this;
+};
+Date.prototype.addMonths = function (months) {
+    this.setMonth(this.getMonth() + months);
+    return this;
+};
+Date.prototype.addYears = function (years) {
+    this.setYear(this.getYear() + years);
+    return this;
+};
 Date.fromJSON = function (jsonDate) {
     try {
         return new Date(parseFloat(jsonDate.replace('/Date(', '').replace(')/', '')));
